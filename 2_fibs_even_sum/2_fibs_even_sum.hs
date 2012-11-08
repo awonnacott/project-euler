570bfe682cc8a79d0eff45de0ef6fc59e376b62c
Andrew <quadcorder@gmail.com>
2012-11-07 22:50:53 -0500

    Integer to integral

--- 2_fibs_even_sum/2_fibs_even_sum.hs
+++ 2_fibs_even_sum/2_fibs_even_sum.hs
@@ -1,12 +1,12 @@
-fib :: Integer -> Int
+fib :: Integral a => a -> Int
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 fibs :: [Int]
 fibs = [fib x | x <- [1..]]
-getUpTo :: [Int] -> Int -> Int -> Int
+getUpTo :: Integral a => [a] -> a -> a -> a
 getUpTo list1 x n = if (list1 !! n) < x then getUpTo list1 x (n+1) else n-1
 relevantNumbers :: [Int]
 relevantNumbers = [fibs !! x | x <- [1..(getUpTo fibs 4000000 0)], even x, even (fibs !! x)]
 main :: IO ()
-main = putStrLn (show (sum relevantNumbers))
+main = show .sum relevantNumbers
