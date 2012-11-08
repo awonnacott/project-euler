9431474d6a61108cfc2dedf771670dde4dba7455
Andrew <quadcorder@gmail.com>
2012-11-07 22:55:05 -0500

    Pointfree show

--- 4_products_palindrome/4_products_palindrome.hs
+++ 4_products_palindrome/4_products_palindrome.hs
@@ -3,6 +3,6 @@ products = [x * y | x <- [100..999], y <- [100..999]]
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome x = (reverse x) == x
 palindromeProducts :: Integral a => [a]
-palindromeProducts = [x | x <- products, isPalindrome (show x)]
+palindromeProducts = [x | x <- products, isPalindrome . show x]
 main :: IO ()
 main = print . maximum palindromeProducts
