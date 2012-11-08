b74c99cc07818b6b81d17e8f287564fb869ba86d
Andrew <quadcorder@gmail.com>
2012-11-07 22:58:13 -0500

    Integer to integral

--- 7_nth_prime/7_nth_prime.hs
+++ 7_nth_prime/7_nth_prime.hs
@@ -1,8 +1,8 @@
-factors :: Integer -> [Integer]
+factors :: Integral a => a -> [Int]
 factors n = [x | x <- [1..n], (mod n x) == 0, n /= x]
-prime :: Integer -> Bool
+prime :: Integral a => a -> Bool
 prime x = if factors x == [1] then True else False
-primes :: [Integer]
+primes :: [Int]
 primes = [x | x <- [1..], prime x]
 main :: IO ()
-main = putStrLn (show (primes !! 10001))
+main = print . primes !! 10001
