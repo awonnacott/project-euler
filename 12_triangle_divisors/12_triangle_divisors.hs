eb4e8b1ed516c9b7c7262de1b3dd147181f6feb0
Andrew <quadcorder@gmail.com>
2012-11-07 23:03:57 -0500

    Integer to integral

--- 12_triangle_divisors/12_triangle_divisors.hs
+++ 12_triangle_divisors/12_triangle_divisors.hs
@@ -1,14 +1,14 @@
-factors :: Integer -> [Integer]
+factors :: Integral a => a -> [a]
 factors n = [x | x <- [1..n], (mod n x) == 0, n /= x]
-howManyFactors :: Integer -> Int
+howManyFactors :: Integral a => a -> Int
 howManyFactors x = length (factors x)
-triangle :: Integer -> Integer
+triangle :: Integral a => a -> a
 triangle n = if n == 0 then n else n + triangle (n - 1)
-triangles :: [Integer]
+triangles :: Integral a => [a]
 triangles = [triangle x | x <- [1..]]
-getFirstPast :: (Integer -> Int) -> [Integer] -> Int -> Int -> Int
+getFirstPast :: Integral a => (a -> Int) -> [a] -> Int -> Int -> Int
 getFirstPast f l x i = if f (l !! i) > x then i else getFirstPast f l x (i + 1)
-trianglePast :: Integer
+trianglePast :: Integral a => a
 trianglePast = triangles !! (getFirstPast (howManyFactors) triangles 500 0)
 main :: IO ()
-main = putStrLn (show trianglePast)
+main = print trianglePast
