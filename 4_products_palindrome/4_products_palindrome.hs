f72a560c2a6d28bb417444e21fc87a4f94859b80
Andrew <quadcorder@gmail.com>
2012-11-07 22:54:09 -0500

    Integer to integral

--- 4_products_palindrome/4_products_palindrome.hs
+++ 4_products_palindrome/4_products_palindrome.hs
@@ -1,8 +1,8 @@
-products :: [Integer]
+products :: Integral a => [a]
 products = [x * y | x <- [100..999], y <- [100..999]]
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome x = (reverse x) == x
-palindromeProducts :: [Integer]
+palindromeProducts :: Integral a => [a]
 palindromeProducts = [x | x <- products, isPalindrome (show x)]
 main :: IO ()
-main = putStrLn (show (maximum palindromeProducts))
+main = print . maximum palindromeProducts
