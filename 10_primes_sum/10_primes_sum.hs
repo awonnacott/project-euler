e0fd1718d9adcf491203aff3881805c3d640bb7b
Andrew <quadcorder@gmail.com>
2012-11-07 23:02:01 -0500

    Integer to integral

--- 10_primes_sum/10_primes_sum.hs
+++ 10_primes_sum/10_primes_sum.hs
@@ -1,12 +1,12 @@
-factors :: Int -> [Int]
+factors :: Integral a => a -> [a]
 factors n = [x | x <- [1..n], (mod n x) == 0, n /= x]
-prime :: Int -> Bool
+prime :: Integral a => a -> Bool
 prime x = factors x == [1]
-primes :: [Int]
+primes :: Integral a => [a]
 primes = [x | x <- [1..], prime x]
 getUpTo :: [Int] -> Int -> Int -> Int
 getUpTo list1 x n = if list1 !! n < x then getUpTo list1 x (n+1) else n-1
 relevantNumbers :: [Int]
 relevantNumbers = [primes !! x | x <- [1..(getUpTo primes 2000000 0)]]
 main :: IO ()
-main = putStrLn (show (sum relevantNumbers))
+main = print . sum relevantNumbers
