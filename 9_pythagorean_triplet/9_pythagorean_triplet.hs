6ffce98320dd5bc84e252d3ee3e685c0f64993cf
Andrew Wonnacott <tricorder.andrewjw@gmail.com>
2019-06-12 12:39:25 -0400

    Update 9_pythagorean_triplet.hs

--- 9_pythagorean_triplet/9_pythagorean_triplet.hs
+++ 9_pythagorean_triplet/9_pythagorean_triplet.hs
@@ -5,7 +5,7 @@ snd3 (x, y, z) = y
 lst3 :: (a, b, c) -> c
 lst3 (x, y, z) = z
 triples :: Integral a => [(a, a, a)]
-triples = [(a, b, c) | a <- [1..], b <- [1..], c <- [1..], a^2+b^2==c^2]
+triples = [(a, b, c) | a <- [1..], b <- [1..a], c <- [1..b], a^2+b^2==c^2]
 triple :: Integral a => (a, a, a)
 triple = head [n | n <- triples, (fst3 n)+(snd3 n)+(lst3 n) == 1000]
 main :: IO ()
